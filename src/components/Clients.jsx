5b20fbfa04d576336dbadc0ddafdf75bd5395244
safchik <safchik9@gmail.com>
2023-11-14 19:18:52 +0000

    logic for footer component added

--- src/components/Clients.jsx
+++ src/components/Clients.jsx
@@ -6,7 +6,7 @@ const Clients = () => (
         <div className={`${styles.flexCenter} flex-wrap w-full`}>
             {clients.map((client) => (
                 <div key={client.id} className={`flex-1 ${styles.flexCenter} sm:min-w-[192px] min-w-[120px] m-5`}>
-                    <img src={client.logo} alt="client_logo" className="sm:w-[192px] w-[100px] object-contain" />
+                    <img src={client.logo} alt="client_logo" className="sm:w-[192px] w-[100px] object-contain hover:brightness-200 transition duration-300 cursor-pointer" />
                 </div>
             ))}
         </div>
